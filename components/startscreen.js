dcf55f0be05c94d0eaea40332f75517400e761ea
Uwechia, Ebubechukwu <euwechia3@gatech.edu>
2024-01-31 14:10:01 -0500

    delete

--- components/startscreen.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {View, Text, Button, Stylesheet} from "react-native";
-
-const startscreen = ({navigation}) => {
-    <><h1>Welcome, sign in</h1>
-    <Button /> wow </>
-}
-
-export default startscreen;
